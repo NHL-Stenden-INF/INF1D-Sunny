7c440fbfe43454eb8befa27530fccab4b4cc1136
Lucas Wanink <ratelsnake707@outlook.com>
2023-10-09 09:52:03 +0200

    Begin gemaakt aan de pagina waar te koop

---
+++ Website_Sunny/Sunny-pagina's/Waar-te-koop.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href=".//css/Style.css">
+        <title>Waar te koop</title>
+    </head>
+<body>
+    <h1>Waar te koop</h1>
+</body>
+</html>
