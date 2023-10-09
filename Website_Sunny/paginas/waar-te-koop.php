3477609f11f9b05e47665e39649d2993b45b79a2
Lucas Wanink <ratelsnake707@outlook.com>
2023-10-09 10:42:25 +0200

    Map namen aangepast

---
+++ Website_Sunny/paginas/waar-te-koop.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel='stylesheet' href='../css/Style.css'>
+    <title>Waar te koop</title>
+</head>
+<body>
+    
+</body>
+</html>
