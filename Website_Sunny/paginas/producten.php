9f7ef157936fe9b31333a587dd42e4bbd2ee4c1c
bryan potze <bryan.potze@student.nhlstenden.com>
2023-10-09 11:00:35 +0200

    code toegevoegd in productne

---
+++ Website_Sunny/paginas/producten.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Sunny Socks</title>
+        <meta charset="UTF-8">
+        <link href="../css/style.css" style="text/css" rel="stylesheet">
+    </head>
+    <body>
+      <header>
+            <?php
+                include ('../html/header.html');
+            ?>
+        </header>
+        <div class="box">
+            <div class="boxes">
+                <div class="infosok">
+                    <div>uni socks</div>
+                    <div>Sunny</div>
+                    <div class="streep">streep</div>
+                    <div class="kleur">kleur</div>
+                    <div>
+                        <div class="box_blauw"></div>
+                        <div class="box_roze"></div>
+                        <div class="box_geel"></div>
+                        <div class="box_groen"></div>
+                        <div class="box_groen"></div>
+                    </div>
+                    <div>Nu kopen</div>
+                    <div class="streep">streep</div>
+                    <div class="productinfo"></div>
+                    <div class="productinfo"></div>
+                </div>
+            </div>
+            <div class="boxes"></div>
+            <div class="boxes">
+                <img src="../IMG/" alt="">
+            </div>
+        </div>
+        <?php
+        if
+        ?>
+        <footer>
+            <?php
+                include ('../html/footer.html');
+            ?>
+        </footer>
+    </body>
+</html>
