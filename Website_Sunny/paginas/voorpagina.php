1f916d2c437be05f7e6295fd280964f6c3c30a83
Lucas Saathof <lucas.saathof@student.nhlstenden.com>
2023-10-10 11:39:43 +0200

    voorpagina, css header veranderd en eigen css

--- Website_Sunny/paginas/voorpagina.php
+++ Website_Sunny/paginas/voorpagina.php
@@ -3,12 +3,40 @@
     <head>
         <title>Sunny Socks</title>
         <meta charset="UTF-8">
-        <link href="../css/style.css" style="text/css" rel="stylesheet">
+        <link href="../css/Style.css" style="text/css" rel="stylesheet">
     </head>
     <body>
-        <?php
-            include ('../html/mijnheader.html');
-            include ('../html/footer.html');
-        ?>
+        <header id="voorpaginaheader">
+            <div class="header-container">
+                <div class="img-header-div">
+                    <img class="sunnylogo-header-img" alt="sunnylogo-header" src="../IMG/sunny_logos_white.png">
+                </div>
+                <div class="items-container">
+                    <ul>
+                        <li class="header-list"><a href="productenpagina.html"><h3>Producten</h3></a></li>
+                        <li class="header-list"><a href="waarproductenhalen.html"><h3>Dit producten halen</h3></a></li>
+                        <li class="header-list"><a href="Sunny.html"><h3>Over Sunny</h3></a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+            <div class="banner_sok"> 
+                <div>
+                    <img src="../img/groenesokbanner.png" class="banners" alt="groene sok banner">
+                </div>
+                    <div class="tekst-in-banner">
+                        <p id="nieuw-design" class="banner-tekst">
+                            Nieuw Design
+                        </p>
+                        <h1 id="uni-color" class="banner-tekst">
+                                Uni color
+                        </h1>
+                    </div>
+            </div>
+        <footer>
+            <?php
+                include ('../html/footer.html');
+            ?>
+        </footer>
     </body>
 </html>
