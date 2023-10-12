f1af1324d5dffdcfdc99d440367c8950f3e66fe7
Swordec Aeterna <swordecgamer1@gmail.com>
2023-10-12 14:57:41 +0200

    Fixed another issue with voorpagina header

--- Website_Sunny/paginas/voorpagina.php
+++ Website_Sunny/paginas/voorpagina.php
@@ -6,19 +6,10 @@
         <link href="../css/Style.css" style="text/css" rel="stylesheet">
     </head>
     <body>
-        <header id="voorpaginaheader">
-            <div class="header-containervanlucas">
-                <div class="img-header-div">
-                    <img class="sunnylogo-header-img" alt="sunnylogo-header" src="../IMG/sunny_logos_white.png">
-                </div>
-                <div class="items-container">
-                    <ul>
-                        <li class="header-list"><a href="producten.php"><h3>Producten</h3></a></li>
-                        <li class="header-list"><a href="waar-te-koop.php"><h3>Deze producten halen</h3></a></li>
-                        <li class="header-list"><a href="over_ons.php"><h3>Over Sunny</h3></a></li>
-                    </ul>
-                </div>
-            </div>
+        <header>
+        <?php
+                include ('../html/mijnheader.html');
+            ?>
         </header>
             <div class="banner_sok"> 
                 <div>
