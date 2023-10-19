2d7724f0227fb93a51a9ba0c69b8d9415ffd9bff
Lucas Wanink <ratelsnake707@outlook.com>
2023-10-19 11:23:44 +0200

    waar te koop pagina is responsive

--- Website_Sunny/paginas/waar-te-koop.php
+++ Website_Sunny/paginas/waar-te-koop.php
@@ -22,15 +22,15 @@
             <div>
                 <img src="../img/blue_striped.png" alt="Blue Striped" class="imgsok">
                 <div>
-                    <a href="https://hema.nl"><img src="../img/hema.jpg" alt="Hema Logo" class="imgstore"></a>
-                    <a href="https://bol.com"><img src="../img/bol.jpg" alt="Bol.com Logo" class="imgstore"></a>
+                    <a href="https://hema.nl" class="acenter"><img src="../img/hema.jpg" alt="Hema Logo" class="imgstore"></a>
+                    <a href="https://bol.com" class="acenter"><img src="../img/bol.jpg" alt="Bol.com Logo" class="imgstore"></a>
                 </div>
             </div>
             <div>
                 <img src="../img/blue_uni.png" alt="Blue Striped" class="imgsok">
                 <div>
-                    <a href="https://hema.nl"><img src="../img/hema.jpg" alt="Hema Logo" class="imgstore"></a>
-                    <a href="https://bol.com"><img src="../img/bol.jpg" alt="Bol.com Logo" class="imgstore"></a>
+                    <a href="https://hema.nl" class="acenter"><img src="../img/hema.jpg" alt="Hema Logo" class="imgstore"></a>
+                    <a href="https://bol.com" class="acenter"><img src="../img/bol.jpg" alt="Bol.com Logo" class="imgstore"></a>
                 </div>
             </div>
             <div>
