070c4bcad88a47b6af031289c76db92c843159ae
Lucas Wanink <ratelsnake707@outlook.com>
2023-10-19 11:17:21 +0200

    Opnieuw pushen ivm storing idk

--- Website_Sunny/paginas/waar-te-koop.php
+++ Website_Sunny/paginas/waar-te-koop.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8"> 
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <link rel="stylesheet" href="../css/Style.css" type="text/css">
     <title>Waar te koop</title>
     <link rel="icon" href="../img/favicon.png" type="image/png">
