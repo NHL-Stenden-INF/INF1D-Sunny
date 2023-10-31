b2fe0052145e69f0ddd0c2bf82a4b841da429e38
bryan potze <bryan.potze@student.nhlstenden.com>
2023-10-31 10:23:02 +0100

    voorpagina verder maken

--- Website_Sunny/paginas/voorpagina.php
+++ Website_Sunny/paginas/voorpagina.php
@@ -1,77 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Sunny Socks</title>
-        <link rel="icon" href="../img/favicon.png" type="image/png">
-        <meta charset="UTF-8">
-        <link href="../css/Style.css" style="text/css" rel="stylesheet">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
-        <?php
-            include ('../HTML/mijnheader.html');
-        ?>
-            <div class="banner_sok"> 
-                <div>
-                    <img src="../img/groenesokbanner.png" class="banners" alt="groene sok banner">
-                    <div class="tekst-in-banner">
-                        <div><p id="nieuw-design" class="banner-tekst">Nieuw Design</p></div>
-                        <div><p id="tekst_unicolor" class="banner-tekst">Ervaar comfort en stijl nu met onze nieuwe <br> Uni Color socks!</p></div>
-                    </div>
-                    <div></div>
-                </div>
-                <div class="tekst-in-banner">
-                    <!-- <p id="nieuw-design" class="banner-tekst">Nieuw Design</p> -->
-                    <h1 id="uni-color" class="banner-tekst">Uni Color</h1>
-                    <p id="tekst_unicolor" class="banner-tekst">Ervaar comfort en stijl nu met onze nieuwe <br> Uni Color socks!</p>
-                    <div id="bestel-nu">
-                        <a href="../paginas/waar-te-koop.php" id="bestel-knop" class="banner-knop">
-                            Bestel nu!
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="dom-stuk-tekst">
-                <p id="nieuw-design-tekst">Het nieuwe design van Sunny is nu eindelijk hier, <br> de Uni Socks!</p>
-                <div class="plaatjes-grid">
-                    <img src="../img/hand-holding-seeding.png" class="voorpagina-plaatjes" alt="hand" id="hand">
-                    <div class="tekst-bij-plaatjes" id="duurzaam">   
-                        Duurzaam!
-                        <p class="nog-meer-tekst">
-                            De sokken van uni socks worden van portugees katoen gemaakt, werknemers worden dus goed
-                            betaald en het is ook nog is goed voor de aarde! 
-                        </p>
-                    </div>    
-                    <img src="../img/magic-wand.png" class="voorpagina-plaatjes" alt="wand" id="wand"> 
-                    <div class="tekst-bij-plaatjes" id="stylish-design"> 
-                        Stylish design!
-                        <p class="nog-meer-tekst">
-                            Met een modern en veelzijdig ontwerp dat bij elke outfit past
-                        </p>
-                    </div>
-                    <img src="../img/grin.png" class="voorpagina-plaatjes" alt="grin" id="grin"> 
-                    <div class="tekst-bij-plaatjes" id="comfortabel"> 
-                        Comfortabel!
-                        <p class="nog-meer-tekst">
-                            Doordat de sokken van hogekwaliteit katoen zijn gemaakt zitten ze gegarandeerd goed.
-                        </p>
-                    </div>
-                    <img src="../img/palette.png" class="voorpagina-plaatjes" alt="palette" id="palette"> 
-                    <div class="tekst-bij-plaatjes" id="goed-assortiment"> 
-                        Goed assortiment in kleuren!
-                        <p class="nog-meer-tekst">
-                            Kies uit een verscheidenheid aan levendige kleuren om uw 
-                            persoonlijke stijl tot uitdrukking te brengen
-                        </p>
-                    </div
-                </div>
-
-            </div>    
-        <?php
+<head>
+    <link rel="icon" href="../img/favicon.png" type="image/png">
+    <meta charset="UTF-8">
+    <link href="../css/Style.css" style="text/css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Voorpagina</title>
+</head>
+<body>
+    <?php
+        include ('../HTML/mijnheader.html');
+    ?>
+    <div class="banner">
+        <img src="../img/groenesokbanner.png" alt="banner" class="bannerfoto">
+        <div class="banner-tekst">
+            <div class="nieuw-design">nieuw design</div>
+            <div class="uni-color">Uni color</div>
+            <div class="unitekst">Ervaar comfort en stijl nu met onze nieuwe</div>
+            <div>Uni Color socks!</div>
+            <div><a href="productpagina.php"><button class="bestel-nu" type="imput">Bestel nu</button></a></div>
+        </div>
+    </div>
+    <div class="sunnyinformatie">Het nieuwe duurzame design van sunny socks is nu eindelijk hier de uni</div>
+    <div class="sunnyinformatie"> socks!</div>
+    <div class="doos">
+        <div class="dozen">
+        <img src="../img/hand-holding-seeding.png" alt="duurzaam" class="omschrijvingfoto">Duurzaam!
+            <div class="sunnytekstomschrijving">De sokken van uni socks worden van portugees katoen gemaakt, werknemers worden dus goed betaald en het is ook nog is goed voor de aarde!</div>
+        </div>
+        <div class="dozen">
+        <img src="../img/magic-wand.png" alt="Stylish" class="omschrijvingfoto">Stylish design
+            <div class="sunnytekstomschrijving">Met een modern en veelzijdig ontwerp dat bij elke outfit past</div>
+        </div>
+    </div>
+    <div class="doos">
+    <div class="dozen">
+    <img src="../img/grin.png" alt="Comfortabel" class="omschrijvingfoto">Confortabel
+            <div class="sunnytekstomschrijving">Doordat de sokken van hogekwaliteit katoen zijn gemaakt zitten ze gegarandeerd goed.</div>
+        </div>
+        <div class="dozen">
+        <img src="../img/palette.png" alt="Kleuren" class="omschrijvingfoto">Groot assortiment in kleuren
+            <div class="sunnytekstomschrijving">Kies uit een verscheidenheid aan levendige kleuren om uw persoonlijke stijl tot uitdrukking te brengen</div>
+        </div>
+    </div>
+    <?php
             include ('../html/footer.html');
-        ?>
-    </body>
+    ?>
+</body>
 </html>
-
-
